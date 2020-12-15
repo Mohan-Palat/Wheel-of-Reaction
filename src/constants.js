4bfa9cc0a5b672cd04741f1d18576b4f90de6f5d
Lawrence DeMaio <ldemaio121@hotmail.com>
2020-12-15 17:18:02 -0500

    Revise constants

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,15 @@ const VOWELS = ["A", "E", "I", "O", "U"];
 
 const WHEEL_WEDGES = [5000, 'BANKRUPT', 300, 500, 450, 500, 800, 'LOSE', 700, 'FREE', 
     650, 'BANKRUPT', 900, 500, 350, 600, 500, 400, 550, 800, 300, 700, 900, 500]
-    let degree = 7.5;
+    
 
 const WHEEL_VALS = new Map();
+let degree = 7.5;
+
+WHEEL_WEDGES.forEach(wedge => {
+    WHEEL_VALS.set(degree, wedge);
+    degree+=15;
+})
 
     for (let i = 0; i < WHEEL_WEDGES.length; i++){
       WHEEL_VALS.set(degree, WHEEL_WEDGES[i]);
