d9edcfeb1d84bef9643ea18517063f893dfe3185
Lawrence DeMaio <ldemaio121@hotmail.com>
2020-12-15 18:30:51 -0500

    Add free play functionality

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import "./App.css";
 import BoardContainer from "./BoardContainer";
 import InputContainer from "./InputContainer";
 import WheelContainer from "./WheelContainer";
+import {VOWELS, WHEEL_VALS} from "./constants.js"
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    let puzzle = [
-      ["", "", "", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "A", "L", "M", "O", "N", "D", "", "", "", "", ""],
-      ["", "", "", "C", "A", "K", "E", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", "", "", ""],
-    ];
-    let phrase = "ALMOND CAKE";
-    let lettersLeft = phrase.split(" ").join("");
     this.state = {
       round: 1,
       players: {
@@ -23,6 +17,7 @@ class App extends Component {
         scores: [0, 0, 0],
         roundScores: [0, 0, 0],
         currentPlayerIndex: 0,
+        freePlay: false,
         getCurrentPlayer: function () {
           return this.name[this.currentPlayerIndex];
         },
@@ -37,10 +32,10 @@ class App extends Component {
       },
       board: {
         currentSpin: -1,
-        currentCategory: "Food and Drink",
-        currentPhrase: phrase,
-        lettersLeft: lettersLeft,
-        currentPuzzle: puzzle,
+        currentCategory: "",
+        currentPhrase: "",
+        lettersLeft: "test",
+        currentPuzzle: [],
         usedLetters: [],
         revealAll: false,
       },
@@ -125,18 +120,23 @@ class App extends Component {
 
   nextPlayer() {
     let players = this.state.players;
-    players.currentPlayerIndex++;
-    console.log(players);
-    if (players.currentPlayerIndex > 2) {
-      players.currentPlayerIndex = 0;
+
+    if (players.freePlay){
+      console.log(`${this.state.players.getCurrentPlayer()} has used their free play!`);
+      players.freePlay = false;
     }
+    else{
+      players.currentPlayerIndex++;
+      if (players.currentPlayerIndex > 2) 
+        players.currentPlayerIndex = 0;
+    }
+
     this.setState({ players });
   }
 
   inputVowel = (e) => {
     let players = this.state.players;
     let board = this.state.board;
-    const VOWELS = ["A", "E", "I", "O", "U"];
     const letter = this.state.playerInput.toUpperCase();
     if (VOWELS.includes(letter) && !board.usedLetters.includes(letter)) {
       if (players.roundScores[players.currentPlayerIndex] - 250 < 0) {
@@ -160,7 +160,7 @@ class App extends Component {
   inputLetter = (e) => {
     let players = this.state.players;
     let board = this.state.board;
-    const VOWELS = ["A", "E", "I", "O", "U"];
+    
     const letter = this.state.playerInput.toUpperCase();
     board.currentPhrase = board.currentPhrase.toUpperCase();
 
@@ -178,6 +178,7 @@ class App extends Component {
       const rgxp = new RegExp(letter, "g");
       const count = (board.currentPhrase.match(rgxp) || []).length;
       players.scorePoints(board.currentSpin * count);
+      players.freePlay = false;
       board.usedLetters.push(letter);
       board.lettersLeft = board.lettersLeft.split(letter).join("");
       board.currentSpin = -1;
@@ -229,55 +230,31 @@ class App extends Component {
   spinWheel = (landed) => {
   
     let board = this.state.board;
+    let players = this.state.players;
 
     switch (landed) {
       case "BANKRUPT":
-        alert(this.state.players.getCurrentPlayer() + " has gone bankrupt!");
+        console.log(this.state.players.getCurrentPlayer() + " has gone bankrupt!");
         this.bankruptPlayer();
         break;
       case "LOSE":
-        alert(this.state.players.getCurrentPlayer() + " has lost their turn!");
+        console.log(this.state.players.getCurrentPlayer() + " has lost their turn!");
         this.nextPlayer();
         break;
       case "FREE":
+        console.log(this.state.players.getCurrentPlayer() + " has earned a Free Play!");
+        board.currentSpin = 500;
+        players.freePlay = true;
+        this.setState({players, board});
         break;
-
       default:
+        console.log(`${this.state.players.getCurrentPlayer()} has spun for $${landed}!`);
         board.currentSpin = landed;
         this.setState({ board });
     }
   };
   handleAssignSpin = (position) => {
-    console.log("Assign spin position", position);
-    let WHEEL_VALS = new Map();
-    WHEEL_VALS.set(7.5, 5000);
-    WHEEL_VALS.set(22.5, "BANKRUPT");
-    WHEEL_VALS.set(37.5, 300);
-    WHEEL_VALS.set(52.5, 500);
-    WHEEL_VALS.set(67.5, 450);
-    WHEEL_VALS.set(82.5, 500);
-    WHEEL_VALS.set(97.5, 800);
-    WHEEL_VALS.set(112.5, "LOSE");
-    WHEEL_VALS.set(127.5, 700);
-    WHEEL_VALS.set(142.5, "FREE");
-    WHEEL_VALS.set(157.5, 650);
-    WHEEL_VALS.set(172.5, "BANKRUPT");
-    WHEEL_VALS.set(187.5, 900);
-    WHEEL_VALS.set(202.5, 500);
-    WHEEL_VALS.set(217.5, 350);
-    WHEEL_VALS.set(232.5, 600);
-    WHEEL_VALS.set(247.5, 500);
-    WHEEL_VALS.set(262.5, 400);
-    WHEEL_VALS.set(277.5, 550);
-    WHEEL_VALS.set(292.5, 800);
-    WHEEL_VALS.set(307.5, 300);
-    WHEEL_VALS.set(322.5, 700);
-    WHEEL_VALS.set(337.5, 900);
-    WHEEL_VALS.set(352.5, 500);
-
-   let value = WHEEL_VALS.get(position)
-    console.log("Value is ", value)
-
+    let value = WHEEL_VALS.get(position)
     this.spinWheel(value)
   };
 }
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,6 @@ const VOWELS = ["A", "E", "I", "O", "U"];
 const WHEEL_WEDGES = [5000, 'BANKRUPT', 300, 500, 450, 500, 800, 'LOSE', 700, 'FREE', 
     650, 'BANKRUPT', 900, 500, 350, 600, 500, 400, 550, 800, 300, 700, 900, 500]
     
-
 const WHEEL_VALS = new Map();
 let degree = 7.5;
 
@@ -11,11 +10,5 @@ WHEEL_WEDGES.forEach(wedge => {
     WHEEL_VALS.set(degree, wedge);
     degree+=15;
 })
-
-    for (let i = 0; i < WHEEL_WEDGES.length; i++){
-      WHEEL_VALS.set(degree, WHEEL_WEDGES[i]);
-      degree+=15;
-}
-
 
 export { VOWELS, WHEEL_WEDGES, WHEEL_VALS};
